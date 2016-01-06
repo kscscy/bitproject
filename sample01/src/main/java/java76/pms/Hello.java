bd79ab4389783317a75e92b5a788e7689abc1616
kscscy <kscscy@gmail.com>
2016-01-06 17:15:01 +0900

    ohora...

--- sample01/src/main/java/java76/pms/Hello.java
+++ sample01/src/main/java/java76/pms/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hellO...");
+		ohora();
 	}
 
+	private static void ohora() {
+		System.out.println("ohora...");
+	}
 }
